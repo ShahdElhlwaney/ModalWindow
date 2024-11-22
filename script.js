ec8df6197efaed7cc131deee8d474536725f977a
Shahd Elhlwaney <ShahdElhlwaney@example.com>
2024-11-22 07:36:16 +0200

    Creating JS Code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict'
+const modal= document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnsShowWindow=document.querySelectorAll('.show-window');
+const btnCloseModal=document.querySelector('.close-modal');
+const showModal= function (){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal= function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+for(let i=0;i<btnsShowWindow.length;i++)
+{
+    btnsShowWindow[i].addEventListener('click',showModal); 
+}
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal);
+document.addEventListener('keydown',function (e){
+    if(e.key==='Escape' && !modal.classList.contains('hidden'))
+    {
+        closeModal();
+    }
+});
